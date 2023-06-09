42ca2e453f35f3c7bd23a28889760767578a1099
Rafael Magalhães <rafamgpfmaio2023@outlook.com>
2023-06-09 14:42:10 -0300

    Respostas do Teste técnico.

---
+++ QuestaoAberta_1/TesteDev/TesteDev/ClasseMain.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace Principal
+{
+    public class ClasseMain
+    {
+        public static void Main(string[] args)
+        {
+            DefineOrdemDaMatriz(out int ordem, out string[,] matrizQuadrada);
+            InsereElementosDaMatriz(ordem, matrizQuadrada);
+            InformaMatrizCriada(matrizQuadrada);
+            DefineDiagonais(matrizQuadrada, out string[] diagonalPrincipal, out string[] diagonalSecundaria);
+            TrocaDiagonaisEntreSi(matrizQuadrada, diagonalPrincipal, diagonalSecundaria);
+            InverteElementosDasDiagonais(matrizQuadrada, diagonalPrincipal, diagonalSecundaria);
+        }
+
+
+        //Não ficou claro para mim se o objetivo do exercício era trocar as diagonais entre si, fazendo a diagonal principal
+        //ficar na posição da secundária e vice-versa, ou se era para inverter a ordem dos elementos dentro de cada diagonal.
+        //Por causa disso, decidi programar as duas possibilidades. Abaixo, o método troca uma diagonal com a outra.
+        private static void TrocaDiagonaisEntreSi(string[,] matrizQuadrada, string[] diagonalPrincipal, string[] diagonalSecundaria)
+        {
+            int ordemMatriz = matrizQuadrada.GetLength(0);
+            string[,] matrizComDiagonaisTrocadas = (string[,])matrizQuadrada.Clone();
+            for (int i = 0; i < ordemMatriz; i++)
+            {
+                matrizComDiagonaisTrocadas[i, i] = diagonalSecundaria[i];
+                for (int j = 0; j < ordemMatriz; j++)
+                {
+                    if (i + j == ordemMatriz - 1)
+                        matrizComDiagonaisTrocadas[i, j] = diagonalPrincipal[i];
+                }
+            }
+            int count = 0;
+
+            Console.WriteLine("\n\nA matriz com as diagonais trocadas entre si é: \n");
+
+            foreach (string elemento in matrizComDiagonaisTrocadas)
+            {
+                if (count % ordemMatriz == 0 && count != 0)
+                    Console.WriteLine("\n");
+
+                count++;
+                Console.Write(elemento + "  ");
+            }
+        }
+
+        //Abaixo, temos a segunda possibilidade que eu comentei, de modo que as diagonais da matriz terão os elementos
+        //com posição reversa em relação à posição original.
+        private static void InverteElementosDasDiagonais(string[,] matrizQuadrada, string[] diagonalPrincipal, string[] diagonalSecundaria)
+        {
+            diagonalPrincipal = diagonalPrincipal.Reverse().ToArray();
+            diagonalSecundaria = diagonalSecundaria.Reverse().ToArray();
+            int ordemMatriz = matrizQuadrada.GetLength(0);
+            for (int i = 0; i < ordemMatriz; i++)
+            {
+                for (int j = 0; j < ordemMatriz; j++)
+                {
+                    matrizQuadrada[i, i] = diagonalPrincipal[i];
+                    if (i + j == ordemMatriz - 1)
+                        matrizQuadrada[i, j] = diagonalSecundaria[i];
+                }
+            }
+            int count = 0;
+
+            Console.WriteLine("\n\nA matriz com os elementos revertidos dentro de cada diagonal é: \n");
+
+            foreach (string elemento in matrizQuadrada)
+            {
+                if (count % ordemMatriz == 0 && count != 0)
+                    Console.WriteLine("\n");
+
+                count++;
+                Console.Write(elemento + "  ");
+            }
+            Console.WriteLine("\n\n\nPrograma finalizado com sucesso!\n");
+        }
+
+        private static void DefineOrdemDaMatriz(out int ordem, out string[,] matrizQuadrada)
+        {
+            while (true)
+            {
+                Console.Write("Informe a ordem da matriz quadrada: ");
+                try
+                {
+                    ordem = Convert.ToInt32(Console.ReadLine());
+                    matrizQuadrada = new string[ordem, ordem];
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("A ordem precisa ser um número inteiro. Corrija o valor informado.\n");
+                }
+            }
+        }
+
+        private static void InsereElementosDaMatriz(int ordem, string[,] matrizQuadrada)
+        {
+            for (int i = 1; i <= ordem; i++)
+            {
+                Console.WriteLine($"Escreva os elementos da linha {i} separados por um espaço em branco: ");
+                string[] linhaDaMatriz = new string[ordem];
+                linhaDaMatriz = Console.ReadLine().Split(" ");
+                if (linhaDaMatriz.Length != ordem)
+                {
+                    Console.WriteLine("\nO número de elementos da linha precisa corresponder à ordem da matriz. Tente novamente.");
+                    i--;
+                    continue;
+                }
+                for (int j = 0; j < ordem; j++)
+                {
+                    matrizQuadrada[i - 1, j] = linhaDaMatriz[j];
+                }
+            }
+        }
+
+        private static void InformaMatrizCriada(string[,] matrizQuadrada)
+        {
+            Console.WriteLine("\nA matriz que você informou é: \n");
+            int ordemMatriz = matrizQuadrada.GetLength(0);
+            int count = 0;
+            foreach (string elemento in matrizQuadrada)
+            {
+                if (count % ordemMatriz == 0 && count != 0)
+                    Console.WriteLine("\n");
+
+                Console.Write(elemento + "  ");
+                count++;
+            }
+        }
+
+        private static void DefineDiagonais(string[,] matrizQuadrada, out string[] diagonalPrincipal, out string[] diagonalSecundaria)
+        {
+            int ordemMatriz = matrizQuadrada.GetLength(0);
+            diagonalPrincipal = new string[ordemMatriz];
+            for (int i = 0; i < diagonalPrincipal.Length; i++)
+            {
+                diagonalPrincipal[i] = matrizQuadrada[i, i];
+            }
+            diagonalSecundaria = new string[ordemMatriz];
+            for (int j = 0; j < ordemMatriz; j++)
+            {
+                for (int k = 0; k < ordemMatriz; k++)
+                {
+                    if (j + k == ordemMatriz - 1)
+                        diagonalSecundaria[j] = matrizQuadrada[j, k];
+                }
+            }
+        }
+    }
+}
---
+++ QuestaoAberta_2/TestDev2/TestDev2/ClassMain.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+namespace Principal
+{
+    public class ClassMain
+    {
+        public static void Main(string[] args)
+        {
+            int ordemMatrizPrincipal = 0;
+            string[][,] vetorDeMatrizes = new string[2][,];
+
+            for (int i = 0; i < 2; i++)
+            {
+                string nomeMatriz = string.Empty;
+                if (i == 0)
+                    nomeMatriz = "A";
+                else
+                    nomeMatriz = "B (submatriz)";
+
+                string[,] matrizQuadrada;
+                int ordem;
+
+                if (i == 0)
+                {
+                    DefineOrdemDaMatriz(out ordem, out matrizQuadrada, nomeMatriz);
+                    ordemMatrizPrincipal = ordem;
+                }
+                else
+                    DefineOrdemDaMatriz(out ordem, out matrizQuadrada, nomeMatriz, ordemMatrizPrincipal);
+
+                InsereElementosDaMatriz(ordem, ref matrizQuadrada, nomeMatriz);
+                vetorDeMatrizes[i] = matrizQuadrada;
+                InformaMatrizCriada(matrizQuadrada, nomeMatriz);
+            }
+
+            CalculaNumeroDeVezes(vetorDeMatrizes);
+        }
+
+        private static void CalculaNumeroDeVezes(string[][,] vetorDeMatrizes)
+        {
+            string[,] matrizPrincipal = vetorDeMatrizes[0];
+            string[,] submatriz = vetorDeMatrizes[1];
+            string primeiroElementoSubmatriz = submatriz[0, 0];
+            List<int[]> primeirosElementosIguais = new List<int[]>();
+            int ordemMatriz = matrizPrincipal.GetLength(0);
+
+            for (int i = 0; i < ordemMatriz; i++)
+            {
+                for (int j = 0; j < ordemMatriz; j++)
+                {
+                    if (matrizPrincipal[i, j].Equals(primeiroElementoSubmatriz))
+                    {
+                        int[] posicao = new int[2];
+                        posicao[0] = i;
+                        posicao[1] = j;
+                        primeirosElementosIguais.Add(posicao);
+                    }
+                }
+            }
+
+            int numeroSubmatrizes = 0;
+
+            foreach (int[] elementoIgual in primeirosElementosIguais)
+            {
+                int posicaoX = elementoIgual[0];
+                int posicaoY = elementoIgual[1];
+                int count = 0;
+
+                for (int i = 0; i < submatriz.GetLength(0); i++)
+                {
+                    if (i > 0 && i < matrizPrincipal.GetLength(0) - 1)
+                    {
+                        posicaoX++;
+                        posicaoY = elementoIgual[1];
+                    }
+
+                    for (int j = 0; j < submatriz.GetLength(0); j++)
+                    {
+                        if (j > 0 && j < matrizPrincipal.GetLength(0) - 1)
+                            posicaoY++;
+
+                        if (posicaoX == matrizPrincipal.GetLength(0) || posicaoY == matrizPrincipal.GetLength(0))
+                            break;
+
+                        if (matrizPrincipal[posicaoX, posicaoY].Equals(submatriz[i, j]))
+                            count++;
+
+                        if (count == submatriz.Length)
+                            numeroSubmatrizes++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"A submatriz B cabe {numeroSubmatrizes} vez(es) na matriz A.");
+        }
+
+        private static void DefineOrdemDaMatriz(out int ordem, out string[,] matrizQuadrada, string nomeMatriz, int ordemMatrizPrincipal = 0)
+        {
+            while (true)
+            {
+                Console.Write($"Informe a ordem da matriz quadrada {nomeMatriz}: ");
+
+                try
+                {
+                    ordem = Convert.ToInt32(Console.ReadLine());
+
+                    if (nomeMatriz.Equals("B (submatriz)") && ordem >= ordemMatrizPrincipal)
+                    {
+                        Console.WriteLine("A submatriz deve ter ordem menor do que a matriz A. Tente mais uma vez.");
+                        continue;
+                    }
+
+                    matrizQuadrada = new string[ordem, ordem];
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("A ordem precisa ser um número inteiro positivo. Corrija o valor informado.\n");
+                }
+            }
+        }
+
+        private static void InsereElementosDaMatriz(int ordem, ref string[,] matrizQuadrada, string nomeMatriz)
+        {
+            for (int i = 1; i <= ordem; i++)
+            {
+                Console.WriteLine($"Escreva os elementos da linha {i} da matriz {nomeMatriz} separados por um espaço em branco: ");
+                string[] linhaDaMatriz = new string[ordem];
+                linhaDaMatriz = Console.ReadLine().Split(" ");
+
+                if (linhaDaMatriz.Length != ordem)
+                {
+                    Console.WriteLine("\nO número de elementos da linha precisa corresponder à ordem da matriz. Tente novamente.");
+                    i--;
+                    continue;
+                }
+
+                for (int j = 0; j < ordem; j++)
+                {
+                    matrizQuadrada[i - 1, j] = linhaDaMatriz[j];
+                }
+            }
+        }
+
+        private static void InformaMatrizCriada(string[,] matrizQuadrada, string nomeMatriz)
+        {
+            Console.WriteLine($"\nA matriz {nomeMatriz} que você informou é: \n");
+            int ordemMatriz = matrizQuadrada.GetLength(0);
+            int count = 0;
+
+            foreach (string elemento in matrizQuadrada)
+            {
+                if (count % ordemMatriz == 0 && count != 0)
+                    Console.WriteLine("\n");
+
+                Console.Write(elemento + "  ");
+                count++;
+            }
+
+            Console.WriteLine("\n");
+        }
+
+    }
+}
